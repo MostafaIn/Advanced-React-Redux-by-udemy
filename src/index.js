41d23e3d3cd347556db6780b3579623ee55f31d8
MostafaIn <mostafa.hazareh@integrify.io>
2019-09-26 15:20:04 +0300

    react router

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import Root from 'reducers/Root';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 ReactDOM.render(
     <Root>
+    <Router>
+        <Route path="/" component={App} />
+    </Router>
         <App />
     </Root>
     , document.querySelector('#root'));
